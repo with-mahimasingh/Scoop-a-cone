55eaabf48f8598960924fa9fed063d237912ef2a
with-mahimasingh <mahimasingh503@gmail.com>
2021-09-14 10:26:12 +0530

    Added Themes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,22 +1,23 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
     final int days=30;
-
+    
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
          centerTitle: true,
-       title: Text('Catalogue App'),
+       title: Text('Scoope A Cone'),
        ),
         body: Center(
           child:Container(
       child: Text('Learning Flutter Day: $days'),
           )
         ),
-        drawer: Drawer(),
+        drawer: MyDrawer(),
     );
   }
 }
